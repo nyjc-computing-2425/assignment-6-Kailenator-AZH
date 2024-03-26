acf843aee59369cc659bddf6ea5af6f1470918bf
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-03-26 11:38:03 +0000

    Finished submission

--- main.py
+++ main.py
@@ -3,18 +3,77 @@ import time
 
 
 # Part 1
-def clock(n):
-    # Your code here
-    pass
+def clock(n: int) -> None:
+  """
+  Updates a clock for each seconds for a specified number of seconds
+
+  Parameters
+  ------------
+  n: int
+    The number of seconds to update the clock by
+
+  Returns
+  -------------
+  None
+  """
+  counter = 0
+
+  while counter<n:      
+    hour = datetime(1,1,1,12,59,59-counter).hour
+    minute = datetime(1,1,1,12,59,59-counter).minute
+    second = datetime(1,1,1,12,59,59-counter).second
+    print(f"{hour}:{minute}:{second}", end='\r')
+    time.sleep(1)
+    counter += 1
 
 
 # Part 2
-def lcm(a, b):
-    # Your code here
-    pass
+def lcm(a: int, b: int) -> int:
+  """
+  Accepts 2 integer inputs and returns the lower common multiple of them
+
+  Parameters
+  ------------
+  a: int
+    1st integer
+  b: int
+    2nd integer
+
+  Returns
+  -------------
+  int
+    The lcm of both numbers
+  """
+  ori_a = a
+  ori_b = b
+  while a != b:
+    if a<b:
+      a += ori_a
+    else:
+      b += ori_b
+  return a
 
 
 # Part 3
-def gcf(a, b):
-    # Your code here
-    pass
+def gcf(a: int, b: int) -> int:
+  """
+  Accepts 2 integer inputs and returns the greatest common factor of them
+
+  Parameters
+  ------------
+  a: int
+    1st integer
+  b: int
+    2nd integer
+
+  Returns
+  -------------
+  int
+    The gcf of both numbers
+  """
+  while b != 0:
+    r = a%b
+    a,b = b,r
+  return a
+
+#clock(3)
